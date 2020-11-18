f6a9375381c31ab95b5247551ae628d4aaec1502
Jgamez17 <jpgamez1789@icloud.com>
2020-11-18 09:37:06 -0500

    commit tree 7affdd2064071f5feeec09f06f297894645ec793

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function fetchImages() {
+    return fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => renderImages(json));
+  }
+  
+function renderImages(imgURLs) {
+    imgURLs["message"].forEach(displayImgs)
+
+    function displayImgs(item) {
+        document.getElementById("dog-image-container").innerHTML += `<img src="${item}">`
+    }
+}
